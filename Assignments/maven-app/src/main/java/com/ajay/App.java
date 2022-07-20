58e6697ad25eabb707e0668f8a304262c3e8cb79
Ajaydeep Singh <ajaydeep1717@gmail.com>
2022-07-20 15:43:57 -0400

    added assignment 1

---
+++ Assignments/maven-app/src/main/java/com/ajay/App.java
@@ -0,0 +1,14 @@
+package com.ajay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	System.out.println( "Welcome! " )
+        System.out.println( "Maven Project - Assignment 1" );
+    }
+}
