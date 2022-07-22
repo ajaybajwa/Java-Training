ab24ac45fc9fc4ad688d4832b260c7555ddc93a2
Ajaydeep Singh <ajaydeep1717@gmail.com>
2022-07-22 19:33:57 -0400

    Assignment 4 added

---
+++ Assignments/Assignment-four/src/main/java/com/hcl/Assignment_four/LinkedListIterate.java
@@ -0,0 +1,20 @@
+package com.hcl.Assignment_four;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListIterate {
+
+	public static void main(String[] args) {
+		List<Integer> list = new LinkedList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+		System.out.println("Full List: " + list.toString());
+
+		System.out.print("From index 4: ");
+		Iterator i = list.listIterator(4);
+		while (i.hasNext()) {
+			System.out.print(" " + i.next());
+		}
+	}
+}
